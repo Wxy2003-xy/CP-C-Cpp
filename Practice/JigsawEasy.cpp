3dc97ed43cd0dc4816f6c6b9080148ec7d8e83e7
Wxy2003-xy <auferstanden1@gmail.com>
2024-12-12 17:14:15 +0800

    12/12

--- Practice/JigsawEasy.cpp
+++ Practice/JigsawEasy.cpp
@@ -1,5 +1,33 @@
 #include<iostream>
+#include<vector>
+#include<math.h>
+#include<sstream>
+using namespace std;
+
+bool isSqure(int x) {
+    long long sr = sqrt(x);
+    return (x % 2 != 0 && sr * sr == x);
+}
 
 int main() {
-    
+    int t; 
+    cin >> t;
+
+    while (t--) { 
+        int n; 
+        cin >> n;
+        int tmp;
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> tmp;
+            sum += tmp;
+            if (isSqure(tmp)) {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+
+    return 0;
 }
